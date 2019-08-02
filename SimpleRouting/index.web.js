b5639ea008bc9aa84f9bdb61e6fdde3d13c198e1
Otto Grajeda <ottograjeda@gmail.com>
2019-08-02 13:52:21 -0700

    Update

---
+++ SimpleRouting/index.web.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import App from './app/components/App'; 
+
+// Render is @ App = not needed here
+//ReactDOM.render(<App/>, document.getElementById('root')); // is loaded @ app/web/index.js
